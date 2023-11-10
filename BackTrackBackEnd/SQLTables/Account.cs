a82609700ed730bbb443a678ec24cd05dcd1c4ba
HAWKSTER7337 <109770480+HAWKSTER7337@users.noreply.github.com>
2023-11-10 15:31:56 -0500

    Initial Commit

---
+++ BackTrackBackEnd/SQLTables/Account.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackTrackBackEnd.SQLTables;
+
+/// <summary>
+/// The Account Column information
+/// </summary>
+public class Account
+{
+    /// <summary>
+    /// The unique identifier for each account
+    /// </summary>
+    [Key]
+    public Guid Id { get; set; }
+    
+    /// <summary>
+    /// The Username of the Account
+    /// </summary>
+    [Required]
+    [StringLength(100)]
+    public string Username { get; set; }
+    
+    /// <summary>
+    /// The Password of the Account
+    /// </summary>
+    [Required]
+    [StringLength(100)]
+    public string Password { get; set; }
+}
